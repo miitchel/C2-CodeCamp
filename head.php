6ec8988d01e6bb381800a5bac99ac632a6a33f83
Sam <sambrands2001@gmail.com>
2021-10-08 14:36:01 +0200

    Create head.php
    head.php gevuld met een link naar de CSS en meta-informatie

---
+++ head.php
@@ -0,0 +1,5 @@
+<?php require_once 'backend/config.php' ?>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
