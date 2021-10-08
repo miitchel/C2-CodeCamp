83cc7705bfca643c24b50b22afd8c025334857eb
ToonBrocks <d284469@edu.rocwb.nl>
2021-10-08 15:03:52 +0200

    Header done

--- Header.php
+++ Header.php
@@ -1,11 +1,14 @@
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
+    <?php require_once('head.php'); ?>
 </head>
 
-<body>
-    <div class="wrapper">
-
+    <div class="header-wrapper">
+        <div class="center">
+            <h1>CodeCamp</h1>
+            <nav>
+                <a href="">Songteksten</a>
+                <a href="">Films</a>
+                <a href="">Gereedschap</a>
+            </nav>
+        </div>
     </div>
-</body>
