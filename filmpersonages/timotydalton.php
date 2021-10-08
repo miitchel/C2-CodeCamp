2177bc811d711e96f4789c6bffe4e867357776be
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2021-10-08 15:11:53 +0200

    informatiepagina over filmpersonages
    Informatiepagina's gemaakt over de filmpersonages

---
+++ filmpersonages/timotydalton.php
@@ -0,0 +1,14 @@
+<header>
+    <?php
+    require_once('../header.php')
+    ?>
+</header>
+    <title>Timoty Dalton</title>
+</head>
+<body>
+    <div class="timotydalton"></div>
+        <h1>Informatie over Timoty Dalton</h1>
+        <p>Dalton groeide op in Manchester. Nadat hij zijn school had afgemaakt, ging hij naar het National Youth Theatre.</p>
+        <p>Hij studeerde twee jaar lang aan de Royal Academy of Dramatic Art. In 1966 ging hij naar het Birmingham Repertory Theatre, waar hij hoofdrollen speelde.</p>
+    </div>
+</body>
