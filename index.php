2928e95e0a7a8c2b2432f8c6ade159875174b6e0
Sam <sambrands2001@gmail.com>
2021-10-11 09:19:05 +0200

    Header geïmporteerd

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
   <?php require_once 'head.php' ?>
   <title>CodeCamp</title>
 </head>
+
 <body>
-  
+  <header>
+    <?php require_once 'header.php' ?>
+  </header>
+
 </body>
+
 </html>
