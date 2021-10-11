feb0d38e1de0abf0382b26aa76f9b15421364955
Sam <sambrands2001@gmail.com>
2021-10-11 09:00:54 +0200

    Update Header.php
    Head content verwijderd en wrapper class veranderd

--- Header.php
+++ Header.php
@@ -1,11 +1,3 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
-</head>
+<div class="header-wrapper">
 
-<body>
-    <div class="wrapper">
-
-    </div>
-</body>
+</div>
