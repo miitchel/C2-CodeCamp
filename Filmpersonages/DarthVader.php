6623aae143a8153b02f1ba22b328f1f3f04b2859
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2021-10-12 14:03:18 +0200

    code van informaties pagina's netter gemaakt

--- Filmpersonages/DarthVader.php
+++ Filmpersonages/DarthVader.php
@@ -5,18 +5,17 @@
     <title>Darth Vader</title>
 </head>
 <body class="darthvader">
-    <header>
-        <?php require_once '../header.php'; ?>
-   </header>
+<header>
+    <?php require_once '../header.php'; ?>
+</header>
     
-   <main>
-       <div class="wrapper">
-            <h1>Informatie over Darth Vader</h1>
-            <p>Darth Vader is een personage uit de Star Wars-films. Hij is als Sith-leerling ondergeschikt aan keizer Palpatine, ook bekend als Darth Sidious.</p>
-            <p>In de jaren 1977, 1980 en 1983 werd Darth Vader gespeeld door acteur David Prowse en stuntman Bob Anderson. Zijn stem werd echter ingesproken door James Earl Jones, omdat diens stem veel zwaarder, duisterder en dreigender klonk.</p>
-            <p>Darth Vader is een dienaar van de kwaadaardige keizer Palpatine (Darth Sidious) en een wreed persoon. Hij is vooral te herkennen aan zijn donkere kostuum, zijn zware stem, en zijn enorme omvang. Hij wordt zowel gevreesd door de Rebellen, als door zijn eigen troepen.</p>
-       </div>
-   </main>
+<main>
+    <div class="wrapper">
+        <h1>Informatie over Darth Vader</h1>
+        <p>Darth Vader is een personage uit de Star Wars-films. Hij is als Sith-leerling ondergeschikt aan keizer Palpatine, ook bekend als Darth Sidious.</p>
+        <p>In de jaren 1977, 1980 en 1983 werd Darth Vader gespeeld door acteur David Prowse en stuntman Bob Anderson. Zijn stem werd echter ingesproken door James Earl Jones, omdat diens stem veel zwaarder, duisterder en dreigender klonk.</p>
+        <p>Darth Vader is een dienaar van de kwaadaardige keizer Palpatine (Darth Sidious) en een wreed persoon. Hij is vooral te herkennen aan zijn donkere kostuum, zijn zware stem, en zijn enorme omvang. Hij wordt zowel gevreesd door de Rebellen, als door zijn eigen troepen.</p>
+    </div>
+</main>
 </body>
 </html>
-
--- Filmpersonages/LukeSkywalker.php
+++ Filmpersonages/LukeSkywalker.php
@@ -1,12 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once '../head.php'; ?>
+    <title>Luke Skywalker</title>
+</head>
+<body class="lukeskywalker">
 <header>
     <?php require_once '../header.php'; ?>
 </header>
-    <title>Luke Skywalker</title>
-    <?php require_once '../head.php'; ?>
-</head>
-<body class="lukeskywalker">
-    <h1>Informatie over Luke Skywalker</h1>
-    <p>Luke Skywalker is een personage uit de Star Wars saga, gespeeld door acteur Mark Hamill.</p>
-    <p>Luke is de tweelingbroer van prinses Leia. Hun ouders zijn Padmé Amidala en Anakin Skywalker. Skywalker werd opgevoed door zijn oom Owen Lars en tante Beru Whitesun Lars.</p>
     
+<main>
+    <div class="wrapper">
+        <h1>Informatie over Luke Skywalker</h1>
+        <p>Luke Skywalker is een personage uit de Star Wars saga, gespeeld door acteur Mark Hamill.</p>
+        <p>Luke is de tweelingbroer van prinses Leia. Hun ouders zijn Padmé Amidala en Anakin Skywalker. Skywalker werd opgevoed door zijn oom Owen Lars en tante Beru Whitesun Lars.</p>
+    </div>
+</main>
 </body>
+</html>
