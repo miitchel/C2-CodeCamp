e319538fe5bad1e333e3446f14d70b958cc9bce5
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2021-10-12 14:21:36 +0200

    link naar index van filmpersonages
    deze link gaat naar de 'homepage' van filmpersonages waar ze kunnen kiezen tussen de personages

---
+++ filmpersonages/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once '../head.php'; ?>
+    <title>Informatie pagina over filmpersonages</title>
+</head>
+<header>
+    <?php require_once '../header.php'; ?>
+</header>
+<body>
+    <div class="informationpages">
+        <a href="<?php echo $base_url; ?>/lukeskywalker.php">Luke Skywalker</a>
+        <a href="<?php echo $base_url; ?>/darthvader.php">Darth Vader</a>
+        <a href="<?php echo $base_url; ?>/timotydalton.php">Timoty Dalton</a>
+        <a href="<?php echo $base_url; ?>/jamesbond.php">James Bond</a>
+    </div>
+</body>
+</html>
