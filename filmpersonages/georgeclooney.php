fc11988d2e3b563d24c8d41e77606038db8a7971
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2021-10-14 14:13:02 +0200

    pagina george clooney
    pagina met informatie over george clooney gemaakt

---
+++ filmpersonages/georgeclooney.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once '../head.php'; ?>
+    <title>George Clooney</title>
+</head>
+<body class="georgeclooney">
+<header>
+    <?php require_once '../header.php'; ?>
+</header>
+    
+<main>
+    <div class="wrapper">
+        <h1>Informatie over George Clooney</h1>
+        <p>George Timothy Clooney (Lexington, Kentucky, 6 mei 1961) is een Amerikaans acteur, regisseur, scenarioschrijver en producent met meer dan dertig filmprijzen en nominaties op zijn naam.</p>
+        <p>Voor de film Syriana won hij een Oscar voor beste mannelijke bijrol. Tevens won hij als producent een Oscar voor beste film voor Argo.</p>
+        <p>Ook kreeg hij Oscarnominaties voor zijn rollen in Michael Clayton en The Descendants, een European Film Award (voor Good Night, and Good Luck) en meerdere Golden Globes (voor Syriana en O Brother, Where Art Thou?).</p>
+    </div>
+</main>
+</body>
+</html>
