63dd35b771f5e307a0ad88cd2d66ada396072321
ToonBrocks <d284469@edu.rocwb.nl>
2021-10-15 14:11:16 +0200

    font verandert

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
             <a href="<?php echo $base_url; ?>/boeken/index.php">Boeken</a>
             <a href="">Songteksten</a>
             <a href="<?php echo $base_url; ?>/films/index.php">Films</a>
-            <a href="">Gereedschap</a>
+            <a href="<?php echo $base_url; ?>/gereedschap/index.php">Gereedschap</a>
         </nav>
     </div>
 </div>
