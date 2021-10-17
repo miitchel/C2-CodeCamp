47dec611c5e418526602d2004f4f5b9a27a6c777
Sam <sambrands2001@gmail.com>
2021-10-17 16:02:31 +0200

    Songteksten index wordt nu in een lijst getoond
    Dit was eerst gewoon onder elkaar. Ook de code opgeknapt.

--- songteksten/index.php
+++ songteksten/index.php
@@ -10,26 +10,30 @@
   <header>
     <?php require_once '../header.php' ?>
   </header>
-  
-  <main>
-    <div class="wrapper">
-      <?php
+
+  <?php
       $query = "SELECT * FROM songteksten";
       $statement = $conn->prepare($query);
       $statement->execute();
       $songteksten = $statement->fetchAll(PDO::FETCH_ASSOC);
-      ?>
+    ?>
 
-
+  <main>
+    <div class="wrapper">
       <h1>Songteksen</h1>
-      <div class="songteksten-menu">
-        <?php foreach($songteksten as $songtekst): ?>
-        <a href="songtekst.php?id=<?php echo $songtekst['id']; ?>"><?php echo $songtekst['artiest'] ?> - <?php echo $songtekst['titel'] ?></a>
-        <?php endforeach ?>
+
+      <div class="items">
+        <ul>
+          <?php foreach($songteksten as $songtekst): ?>
+          <li>
+            <a href="songtekst.php?id=<?php echo $songtekst['id']; ?>"><?php echo $songtekst['artiest'] ?> - <?php echo $songtekst['titel'] ?></a>
+          </li>
+          <?php endforeach ?>
+        </ul>
       </div>
     </div>
   </main>
-  
+
   <footer>
     <?php require_once '../footer.php' ?>
   </footer>
