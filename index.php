7fa7f952a58889966da6ba1abcd86a9a65c76c34
Sam <sambrands2001@gmail.com>
2021-10-18 11:46:59 +0200

    Previews toegevoegd
    Op de homepagina worden nu 3 previews getoond van willekeurige songteksten, films en gereedschappen.

--- index.php
+++ index.php
@@ -6,19 +6,72 @@
   <title>CodeCamp</title>
 </head>
 
+<?php 
+    require_once 'backend/conn.php';
+    $query = 'SELECT * FROM songteksten';
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $songteksten = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    require_once 'backend/conn.php';
+    $query = 'SELECT * FROM filmpersonages';
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $filmpersonages = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    require_once 'backend/conn.php';
+    $query = 'SELECT * FROM gereedschap';
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $gereedschappen = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <body>
   <header>
     <?php require_once 'header.php' ?>
   </header>
-  
+
   <main>
     <div class="wrapper">
       <div class="clock" id="clock">
         00:00
       </div>
+
+      <div class="content-preview">
+        <div class="preview">
+          <h2>Songtekst</h2>
+          <div class="preview-body">
+            <?php $item = $songteksten[array_rand($songteksten)]; 
+            
+              require 'preview.php';
+            ?>
+            
+          </div>
+        </div>
+
+        <div class="preview">
+          <h2>Film</h2>
+          <div class="preview-body">
+            <?php $item = $filmpersonages[array_rand($filmpersonages)]; 
+              
+              require 'preview.php';
+            ?>
+          </div>
+        </div>
+
+        <div class="preview">
+          <h2>Gereedschap</h2>
+          <div class="preview-body">
+            <?php $item = $gereedschappen[array_rand($gereedschappen)]; 
+              
+              require 'preview.php';
+            ?>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
-  
+
   <footer>
     <?php require_once 'footer.php' ?>
   </footer>
---
+++ preview.php
@@ -0,0 +1,2 @@
+<h3><?php echo $item['titel'] ?? $item['naam']; ?></h3>
+<pre><?php echo $item['beschrijving'] ?? $item['songtekst']; ?></pre>
