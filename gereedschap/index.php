9172a8278908f5e5b85e19523a637d39988e51bd
Sam <sambrands2001@gmail.com>
2021-10-18 09:37:32 +0200

    wrappers hernoemd

--- gereedschap/index.php
+++ gereedschap/index.php
@@ -13,14 +13,14 @@
 
     <main>
         <div class="wrapper">
-            <div class="gereedschap">
+            <div class="sub-wrapper">
                 <h1>Gereedschap</h1>
                 <p>Gereedschappen of werktuigen zijn hulpmiddelen om bewerkingen te kunnen uitvoeren die een mens (of dier) met zijn lichaam zelf niet of moeilijk kan uitvoeren.</p>
                 <p>Mensen en mensapen kunnen gereedschap gebruiken, maar ook bij kraaiachtigen, Goffins kaketoes, zeeotters en dolfijnen is het gebruik van gereedschap gezien.</p>
                 <p>Andere termen die gebruikt worden voor bepaalde typen gereedschap zijn gerei (meervoud van zachter gereedschap) en instrument (over het algemeen een meer geavanceerd of erg specialistisch stuk gereedschap). Voor groter en zwaarder gereedschap wordt meestal de term werktuig gebruikt.</p>
                 <p>Er zijn stukken gereedschap die in verschillende velden worden gebruikt, maar vaak is gereedschap zeer gespecialiseerd voor een bepaalde toepassing.</p>
 
-                <div class="gereedschap-list">
+                <div class="items">
                     <h2>Gereedschap</h2>
                     <a href="gereedschap.php?id=2">Hamers</a>
                     <a href="gereedschap.php?id=5">Schroevendraaiers</a>
