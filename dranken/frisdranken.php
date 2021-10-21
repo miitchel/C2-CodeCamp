0ef7be74e93e41388d902297c3163ca2b503440e
Sam <sambrands2001@gmail.com>
2021-10-21 13:42:55 +0200

    Footer toegevoegd aan dranken pagina's

--- dranken/frisdranken.php
+++ dranken/frisdranken.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php require_once '../head.php'; ?>
-    <title>CodeCamp - Frisdrank</title>
+  <?php require_once '../head.php'; ?>
+  <title>CodeCamp - Frisdrank</title>
 </head>
+
 <body class="frisdrank">
-<header>
+  <header>
     <?php require_once '../header.php'; ?>
-</header>
-    
-<main>
+  </header>
+
+  <main>
     <div class="wrapper">
-        <h1>De frisdranken</h1>
-        <h3>Cola</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, totam ex! Sit iusto ut laudantium distinctio debitis illum necessitatibus deserunt. Ab quibusdam minus quis itaque incidunt, blanditiis nulla tenetur beatae!</p>
-        <h3>Seven Up</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium quos delectus rerum asperiores ipsa libero autem fugiat esse! Ab commodi pariatur repellendus neque soluta hic inventore animi amet veritatis.</p>
-        <h3>Sinas</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptatum amet. Pariatur magnam quae obcaecati perspiciatis veniam at hic sunt dolore nesciunt, quos excepturi nemo ducimus id recusandae quas molestias!</p>
+      <h1>De frisdranken</h1>
+      <h3>Cola</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, totam ex! Sit iusto ut laudantium distinctio debitis illum necessitatibus deserunt. Ab quibusdam minus quis itaque incidunt, blanditiis nulla tenetur beatae!</p>
+      <h3>Seven Up</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium quos delectus rerum asperiores ipsa libero autem fugiat esse! Ab commodi pariatur repellendus neque soluta hic inventore animi amet veritatis.</p>
+      <h3>Sinas</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptatum amet. Pariatur magnam quae obcaecati perspiciatis veniam at hic sunt dolore nesciunt, quos excepturi nemo ducimus id recusandae quas molestias!</p>
     </div>
-</main>
+  </main>
+
+  <footer>
+    <?php require_once '../footer.php'; ?>
+  </footer>
+
 </body>
+
 </html>
--- dranken/warmedranken.php
+++ dranken/warmedranken.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php require_once '../head.php'; ?>
-    <title>CodeCamp - Warme dranken</title>
+  <?php require_once '../head.php'; ?>
+  <title>CodeCamp - Warme dranken</title>
 </head>
+
 <body class="warmedranken">
-<header>
+  <header>
     <?php require_once '../header.php'; ?>
-</header>
-    
-<main>
+  </header>
+
+  <main>
     <div class="wrapper">
-        <h1>De warme dranken</h1>
-        <h3>Koffie</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, veniam non. Impedit quod quam error, iste voluptatum adipisci porro eius aperiam omnis in ullam, corrupti earum qui deleniti repellendus odit.</p>
-        <h3>Latte</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae alias tempore, rerum explicabo suscipit numquam sint voluptas minima optio debitis eius libero molestiae totam dolore ipsa illo quas provident nemo.</p>
-        <h3>Thee</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolor, placeat assumenda voluptates libero nesciunt beatae iure pariatur expedita cumque magnam nisi dolore praesentium architecto, accusantium provident, est voluptate cum.</p>
+      <h1>De warme dranken</h1>
+      <h3>Koffie</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, veniam non. Impedit quod quam error, iste voluptatum adipisci porro eius aperiam omnis in ullam, corrupti earum qui deleniti repellendus odit.</p>
+      <h3>Latte</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae alias tempore, rerum explicabo suscipit numquam sint voluptas minima optio debitis eius libero molestiae totam dolore ipsa illo quas provident nemo.</p>
+      <h3>Thee</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolor, placeat assumenda voluptates libero nesciunt beatae iure pariatur expedita cumque magnam nisi dolore praesentium architecto, accusantium provident, est voluptate cum.</p>
     </div>
-</main>
+  </main>
+  
+  <footer>
+      <?php require_once '../footer.php'; ?>
+  </footer>
+  
 </body>
+
 </html>
