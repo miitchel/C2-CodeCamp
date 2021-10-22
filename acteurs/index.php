4bf1dabbd9e25fdf0717a115e0c8c04f7e91c618
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2021-10-22 14:19:59 +0200

    Index Auteurs
    Begin Index pagina van auteurs toegevoegd

---
+++ acteurs/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <?php require_once '../head.php' ?>
+    <title>CodeCamp - Acteurs/Actrices</title>
+</head>
+
+<body>
+    <header>
+        <?php require_once '../header.php' ?>
+    </header>
+
+    <main>
+        <div class="wrapper">
+            <div class="sub-wrapper">
+                <h1>Acteurs/Actrices</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, optio animi! Hic iste nostrum, deleniti quis cumque, laboriosam doloremque quisquam sunt veritatis commodi necessitatibus assumenda blanditiis quaerat nisi possimus! Facilis eos doloremque fuga eum, vitae sapiente. Distinctio, sequi. Ratione ducimus sapiente, delectus voluptatum hic saepe excepturi beatae iure numquam quas, ut nesciunt expedita in temporibus nisi molestiae libero officiis deleniti! Eaque provident odit consequuntur, iste asperiores voluptate iure veritatis fuga necessitatibus iusto adipisci dolorum aperiam sit laboriosam, harum vel eos animi esse eum aut? Quo cupiditate tempore porro beatae vitae aperiam corrupti cumque ut voluptatem alias. Incidunt ut accusamus dicta.</p>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <?php require_once '../footer.php' ?>
+    </footer>
+
+</body>
+
+</html>
