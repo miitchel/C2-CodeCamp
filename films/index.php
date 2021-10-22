a2fcf05643e14f075d421a4efb1ed99653323084
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2021-10-22 14:06:28 +0200

    film links weggehaald
    links naar imb, videoland en movies weggehaald

--- films/index.php
+++ films/index.php
@@ -17,13 +17,6 @@
                 <h1>Films</h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, optio animi! Hic iste nostrum, deleniti quis cumque, laboriosam doloremque quisquam sunt veritatis commodi necessitatibus assumenda blanditiis quaerat nisi possimus! Facilis eos doloremque fuga eum, vitae sapiente. Distinctio, sequi. Ratione ducimus sapiente, delectus voluptatum hic saepe excepturi beatae iure numquam quas, ut nesciunt expedita in temporibus nisi molestiae libero officiis deleniti! Eaque provident odit consequuntur, iste asperiores voluptate iure veritatis fuga necessitatibus iusto adipisci dolorum aperiam sit laboriosam, harum vel eos animi esse eum aut? Quo cupiditate tempore porro beatae vitae aperiam corrupti cumque ut voluptatem alias. Incidunt ut accusamus dicta.</p>
                 <a href="<?php echo $base_url; ?>/filmpersonages/index.php">Filmpersonages</a>
-                <div class="items">
-                    <ul>
-                        <li><a href="https://www.imdb.com/">IMDB</a></li>
-                        <li><a href="https://www.videoland.com/nl/">Videoland</a></li>
-                        <li><a href="https://dan.com/nl-nl/domeinnaam-kopen/movies.nl">Movies.nl</a></li>
-                    </ul>
-                </div>
             </div>
         </div>
     </main>
