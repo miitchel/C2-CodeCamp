84da434fe193e306f94db52f9a94b28bcb29af50
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2021-10-27 15:09:05 +0200

    home knop
    Home knop toegevoegd aan de header en het project naam de link weggehaald

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
 <div class="header-wrapper">
     <div class="header-center">
-        <a href="<?php echo $base_url; ?>/index.php">
             <h1>CodeCamp</h1>
-        </a>
         <nav>
+            <a href="<?php echo $base_url; ?>/index.php">Home</a>
             <a href="<?php echo $base_url; ?>/boeken/index.php">Boeken</a>
             <a href="<?php echo $base_url; ?>/songteksten/index.php">Songteksten</a>
             <a href="<?php echo $base_url; ?>/films/index.php">Films</a>
