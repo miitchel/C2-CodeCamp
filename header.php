fe71ad5e83a4eca5b4a2023ffa86e341e225e841
Sam <sambrands2001@gmail.com>
2021-11-01 10:23:23 +0100

    Class toegevoegd aan de navigatielinks

--- header.php
+++ header.php
@@ -2,13 +2,13 @@
     <div class="header-center">
             <h1>CodeCamp</h1>
         <nav>
-            <a href="<?php echo $base_url; ?>/index.php">Home</a>
-            <a href="<?php echo $base_url; ?>/boeken/index.php">Boeken</a>
-            <a href="<?php echo $base_url; ?>/songteksten/index.php">Songteksten</a>
-            <a href="<?php echo $base_url; ?>/films/index.php">Films</a>
-            <a href="<?php echo $base_url; ?>/acteurs/index.php">Acteurs/Actrices</a>
-            <a href="<?php echo $base_url; ?>/gereedschap/index.php">Gereedschap</a>
-            <a href="<?php echo $base_url; ?>/dranken/index.php">Dranken</a>
+            <a class="nav-link" href="<?php echo $base_url; ?>/index.php">Home</a>
+            <a class="nav-link" href="<?php echo $base_url; ?>/boeken/index.php">Boeken</a>
+            <a class="nav-link" href="<?php echo $base_url; ?>/songteksten/index.php">Songteksten</a>
+            <a class="nav-link" href="<?php echo $base_url; ?>/films/index.php">Films</a>
+            <a class="nav-link" href="<?php echo $base_url; ?>/acteurs/index.php">Acteurs/Actrices</a>
+            <a class="nav-link" href="<?php echo $base_url; ?>/gereedschap/index.php">Gereedschap</a>
+            <a class="nav-link" href="<?php echo $base_url; ?>/dranken/index.php">Dranken</a>
         </nav>
     </div>
 </div>
