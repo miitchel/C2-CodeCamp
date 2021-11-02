013ab331ad1db1d84c93b0f115d7ac99bcab1c1d
Sam <sambrands2001@gmail.com>
2021-11-02 14:33:19 +0100

    Kaart toegevoegd in de footer

--- footer.php
+++ footer.php
@@ -15,8 +15,9 @@
             </div>
         </div>
         <div class="footer-subwrapper">
+            <h1>Locatie:</h1>
             <div class="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2472.949874384577!2d5.291411215776969!3d51.697362379666785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6ee66bc48c005%3A0x4dddc528356e3d4f!2sKoestraat%201%2C%205223%20AG%20&#39;s-Hertogenbosch!5e0!3m2!1snl!2snl!4v1635760043795!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2472.949874384577!2d5.291411215776969!3d51.697362379666785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6ee66bc48c005%3A0x4dddc528356e3d4f!2sKoestraat%201%2C%205223%20AG%20&#39;s-Hertogenbosch!5e0!3m2!1snl!2snl!4v1635760043795!5m2!1snl!2snl"  style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
         <div class="footer-subwrapper">
